81fab08a8bc8adaea5079f6d81d48c848dd10087
Guilherme <italoguilherme@lapisco.ifce.edu.br>
2024-08-05 18:30:26 -0300

    ok

---
+++ run.sh
@@ -0,0 +1,5 @@
+microk8s kubectl apply -f otel/
+
+microk8s kubectl apply -f infrastructure/
+
+microk8s kubectl apply -f services/
---
+++ stop.sh
@@ -0,0 +1,5 @@
+microk8s kubectl delete -f services/
+
+microk8s kubectl delete -f infrastructure/
+
+microk8s kubectl delete -f otel/
